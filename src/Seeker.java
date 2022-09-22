320c96093788c6cca0f752ce95b8188a172bb3b1
Suresh Nayak <sureshnnayak24@gmail.com>
2022-09-21 19:09:59 -0600

    seeker changes

--- src/Seeker.java
+++ src/Seeker.java
@@ -3,7 +3,40 @@ public class Seeker extends Creature{
         super();
     }
 
-    public void move(String room){
-        self.currentLocation = room;
+    public void move(ArrayList<Room> fecility){
+        Room currentRoom;
+        String nextLocation = null;
+        ArrayList<Room> adjacentRooms;
+        for (Room room : facility) {
+            if (room.id.equals(this.currentLocation)){
+                currentRoom = room;
+                break;
+            }    
+        }
+        /* check if adventurer is present in current room --> return  */
+
+        for (Room room : facility) {
+            if( room.id[0] == this.currentLocation[0]){
+                if (room.isAdventurerPresent()){
+                    this.currentLocation = room.id; // updating the current location in the creature object.
+                    currentRoom.removeCreatureFromList(this.type);  // removing the creture from the current room
+                    room.addCreatureToList(this.type); // adding the creture to next room
+                    break;
+                }
+            }
+        }
+
+
+
+
+  
+
+
+        // find adjacent adventurers in the same level
+        //move to that room 
+        
+
+        // update room status
+        
     }
 }
